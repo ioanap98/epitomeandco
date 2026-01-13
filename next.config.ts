d3a4677fd87a3cbdd02a6dab6bb7d79329efe3df
Ioana Popescu <ioanapopescu1298@gmail.com>
2026-01-13 17:39:21 +0000

    Fixed 18

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ["en-GB", "ro", "en"],
-    defaultLocale: "en-GB",
-    localeDetection: true,
-  },
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
